c677cba27fafb2a5050bfceb07bc47bc1f1489cd
Robi23134 <164391216+Robi23134@users.noreply.github.com>
2024-03-21 21:10:36 +0100

    Create Dis.js

---
+++ Dis.js
@@ -0,0 +1,9 @@
+documento.getElementById("BVer").addEventListener('click', función() {
+    documento.getElementById("resultado").estilo.display = "bloque";
+})
+
+documento.getElementById("BotonCerrar").addEventListener('click', función() {
+    documento.getElementById("resultado").estilo.display = "ninguno";
+    documento.querySelector(". Contenedor-Binicio").estilo.display = "ninguno";
+    documento.querySelector(". Con-2").estilo.display = "bloque";
+})
